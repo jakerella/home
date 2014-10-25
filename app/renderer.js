fd90a3a5c1fd627594c6163d1b38e38064c67511
jakerella <jordan@jordankasper.com>
2014-10-25 10:51:18 -0400

    Refactored renderer a bit

--- app/renderer.js
+++ app/renderer.js
@@ -2,20 +2,16 @@
 var path = require('path'),
     jade = require('jade');
 
-module.exports = {
-
-    build: function (slug, options) {
-        return jade.renderFile(path.join('templates', options.template, 'base.jade'), {
-            title: (options.title || 'My Site'),
-            renderContent: function() {
-                return jade.render(
-                    'include:markdown ' + path.join(options.contentDir, slug + '.md'),
-                    {
-                        filename: 'base'
-                    }
-                );
-            }
-        });
-    }
-
+module.exports = function (slug, options) {
+    return jade.renderFile(path.join('templates', options.template, 'base.jade'), {
+        title: (options.title || 'My Site'),
+        renderContent: function() {
+            return jade.render(
+                'include:markdown ' + path.join(options.contentDir, slug + '.md'),
+                {
+                    filename: 'base'
+                }
+            );
+        }
+    });
 };
--- app/router.js
+++ app/router.js
@@ -1,16 +1,16 @@
 
-var renderer = require('./renderer.js');
+var render = require('./renderer.js');
 
 module.exports = function(app, options) {
 
     // Homepage (index) router
     app.get('/', function(req, res) {
-        res.send(renderer.build('home', options));
+        res.send(render('home', options));
     });
 
     // router for all other pages/posts
     app.get('*', function(req, res) {
-        res.send(renderer.build('home', options));
+        res.send(render('home', options));
     });
 
 };
