9ee7f7c4059595147204fade3a541956db7a64dd
jakerella <jordan@jordankasper.com>
2014-10-26 16:42:09 -0400

    Added better options for content and templates on specific pages

--- app/renderer.js
+++ app/renderer.js
@@ -1,17 +1,24 @@
 
 var path = require('path'),
-    jade = require('jade');
+    jade = require('jade'),
+    _ = require('lodash');
 
 module.exports = function (slug, options) {
-    return jade.renderFile(path.join('templates', options.template, 'base.jade'), {
-        title: (options.title || 'My Site'),
+    var data = options.pageData[slug] || {};
+
+    data.template = data.template || options.defaultTemplate || 'base.jade';
+    data.title = data.title || options.title || 'My Site - %slug%';
+
+    _.extend(data, {
         renderContent: function() {
             return jade.render(
                 'include:markdown ' + path.join(options.contentDir, slug + '.md'),
-                {
-                    filename: 'base'
-                }
+                { filename: 'FOOBAR' } // this option must be here, but is not used
             );
         }
     });
+
+    data.title = data.title.replace(/%slug%/g, slug.replace(/\-/g, ' ').replace(/\w*/g, function(t){ return t.charAt(0).toUpperCase()+t.substr(1).toLowerCase(); }));
+
+    return jade.renderFile(path.join('templates', options.template, data.template), data);
 };
